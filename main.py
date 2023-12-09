0f211df336d5dfbaf9dd3a6495513a89b36c54b3
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-09 13:48:23 +0530

    created  message func using socket

--- main.py
+++ main.py
@@ -55,8 +55,20 @@ def room():
     room=session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
-    return render_template("room.html")
+    return render_template("room.html",code=room,messages=rooms[room]["messages"])
 
+@socketio.on("message")
+def message(data):
+    room=session.get("room")
+    if room not in rooms:
+        return
+    content={
+        "name":session.get("name"),
+        "message":data["data"]
+    }
+    send(content,to=room)  #type:ignore
+    rooms[room]["message"].append(content)
+    print(f"{session.get('name')} said: {data['data']}")
 @socketio.on("connect")
 def connect(auth):
     room=session.get("room")
@@ -69,7 +81,7 @@ def connect(auth):
         return
     
     join_room(room)
-    send({"name": name, "message": "has entered the room"}, to=room)   #type:ignore
+    send({"name": name, "message": " has entered the room"}, to=room)   #type:ignore
     rooms[room]["members"] +=1
     print(f"{name}joined room {room}")
     
