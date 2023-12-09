6adfd7229de394c1df08542c18b423d1f3a47069
Shreyasb1015 <bagweshreyas1015@gmail.com>
2023-12-09 12:39:56 +0530

    created html page for room chat

--- main.py
+++ main.py
@@ -52,6 +52,9 @@ def home():
 
 @app.route("/room")
 def room():
+    room=session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
     return render_template("room.html")
 if __name__=="__main__":
     socketio.run(app,debug=True)   #type:ignore
